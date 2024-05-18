4205b63d45df82f763009d51ba44b0ef3be6d352
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-05-18 15:35:23 +0400

    Add contact and navigation in the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,42 @@
 import React from "react";
-import { Container } from "./style";
+import {
+  ContactContainer,
+  ContactLink,
+  ContactTitle,
+  Container,
+  ContentLink,
+  ContentsContainer,
+  ContentsTitle,
+  ResourceContainer,
+  ResourcesTitle,
+} from "./style";
+import { routes } from "../../constants/routes";
 
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <ContentsContainer>
+        <ContentsTitle>KOTOBA</ContentsTitle>
+        <ContentLink to={routes.home}>Home</ContentLink>
+        <ContentLink to={routes.gradeKanji}>Kanji Types</ContentLink>
+        <ContentLink to={routes.allKanji}>All Kanji</ContentLink>
+        <ContentLink to={routes.about}>About</ContentLink>
+        <ContentLink to={routes.resources}>Resources</ContentLink>
+      </ContentsContainer>
+      <ResourceContainer>
+        <ResourcesTitle>RESOURCES</ResourcesTitle>
+      </ResourceContainer>
+      <ContactContainer>
+        <ContactTitle>CONTACT</ContactTitle>
+        <ContactLink to="mailto:elenedgebuadze83@gmail.com" target="_blank">
+          elenedgebuadze83@gmail.com
+        </ContactLink>
+        <ContactLink as="a" href="https://wa.me/995599783532" target="_blank">
+          WhatsApp
+        </ContactLink>
+      </ContactContainer>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,10 +1,66 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Container = styled.div`
   position: absolute;
   background-color: black;
   width: 100%;
-  margin-top: 150vw;
+  top: 360vh;
   left: 0;
-  height: 300px;
+  height: 320px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ContentsTitle = styled.p`
+  font-size: 28px;
+  color: white;
+  margin-bottom: 5px;
+`;
+
+export const ContentsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 15vw;
+`;
+
+export const ContentLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  margin-top: 20px;
+`;
+
+export const ResourceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 25vw;
+`;
+
+export const ResourcesTitle = styled.p`
+  font-size: 28px;
+  color: white;
+`;
+
+export const ResourceLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  margin-top: 20px;
+`;
+
+export const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 25vw;
+`;
+
+export const ContactTitle = styled.p`
+  font-size: 28px;
+  color: white;
+  margin-bottom: 5px;
+`;
+
+export const ContactLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  margin-top: 20px;
 `;
